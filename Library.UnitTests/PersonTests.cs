ca60f0961d1e1e5025144982b70d44fe954af637
Ruben van Zanten <r.vanzanten@yellax.com>
2026-03-20 14:25:03 +0100

    Add Alcoholtests

--- Library.UnitTests/PersonTests.cs
+++ Library.UnitTests/PersonTests.cs
@@ -2,4 +2,68 @@
 
 public class PersonTests
 {
+    [Fact]
+    public void IsAllowedToBuyAlcohol_ForOnAge_ReturnsTrue()
+    {
+        // Arrange
+        var sut = new Person
+        {
+            Age = 18,
+        };
+
+        // Act
+        var result = sut.IsAllowedToBuyAlcohol();
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+
+    [Fact]
+    public void IsAllowedToBuyAlcohol_ForUnderAge_ReturnsFalse()
+    {
+        // Arrange
+        var sut = new Person
+        {
+            Age = 17,
+        };
+
+        // Act
+        var result = sut.IsAllowedToBuyAlcohol();
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsAllowedToBuyAlcohol_ForNegativeAge_ReturnsFalse()
+    {
+        // Arrange
+        var sut = new Person
+        {
+            Age = -30,
+        };
+
+        // Act
+        var result = sut.IsAllowedToBuyAlcohol();
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsAllowedToBuyAlcohol_ForOverAge_ReturnsTrue()
+    {
+        // Arrange
+        var sut = new Person
+        {
+            Age = 19,
+        };
+
+        // Act
+        var result = sut.IsAllowedToBuyAlcohol();
+
+        // Assert
+        result.Should().BeTrue();
+    }
 }
